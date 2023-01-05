94665c3fcebd6742b32bf0e8c9fd1f9910062ba9
kenanxasiyevv <kananakh@code.edu.az>
2023-01-05 19:32:04 +0400

    Add project files.

---
+++ des/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace des
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Massivin uzunlugunu daxil edin: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]= ");
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            double d =(double)arr[1] - arr[0];
+            double d1;
+            bool hendesisilsiledir = true;
+
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                d1 = arr[i + 1] - arr[i];
+
+                if (d != d1)
+                {
+                    Console.WriteLine("Hendesi silsile deyil");
+                    hendesisilsiledir = false;
+                    break;
+                }
+            }
+                if (hendesisilsiledir)
+                    Console.WriteLine("hendesi silsiledir");
+        }
+    }
+}
